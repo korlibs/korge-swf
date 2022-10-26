cfb29bbdee2d0c955b8b5d20e1ff7e269cefe6de
soywiz <soywiz@gmail.com>
2022-10-26 14:56:04 +0200

    Add tic-tac-toe SWF sample

--- example/src/commonMain/kotlin/Main.kt
+++ example/src/commonMain/kotlin/Main.kt
@@ -1,6 +1,8 @@
 import com.soywiz.korge.*
 import com.soywiz.korge.scene.*
+import samples.tictactoeswf.*
 
 suspend fun main() = Korge {
     sceneContainer().changeTo({ MainSWF() })
+    //sceneContainer().changeTo({ MainTicTacToeSwf() })
 }
